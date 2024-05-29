07df8bc66973a6e3e289cf5c62d8744a2d62d2ec
juanpabloramosp <juanpabloramosp@hotmail.com>
2024-05-28 18:34:43 -0600

    CRUD DAL COmpleted

---
+++ Northwind202402/DAL/Implementations/CategoryDALImpl.cs
@@ -0,0 +1,84 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class CategoryDALImpl : ICategory
+    {
+
+        private NorthWindContext context;
+
+
+        public CategoryDALImpl(NorthWindContext context)
+        {
+            this.context = context;
+        }
+
+
+        public bool Add(Category category)
+        {
+            try
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public Category Get(int id)
+        {
+            return context.Categories.Find(id) ?? new Category();
+        }
+
+        public IEnumerable<Category> Get()
+        {
+            return context.Categories.ToList();
+        }
+
+        public bool Remove(Category category)
+        {
+            try
+            {
+                context.Categories.Attach(category);
+                context.Categories.Remove(category);
+                context.SaveChanges();
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool Update(Category category)
+        {
+            try
+            {
+                context.Entry(category).State = EntityState.Modified;
+               
+                context.SaveChanges();
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+    }
+}
---
+++ Northwind202402/DAL/Interfaces/ICategory.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface ICategory
+    {
+
+        bool Add(Category category);
+        bool Remove(Category category);
+        bool Update(Category category);
+
+        Category Get(int id);
+        IEnumerable<Category> Get();
+
+
+    }
+}
