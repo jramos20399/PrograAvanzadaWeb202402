7a97167f58f67c624a2143319a3f3a23c0c95631
juanpabloramosp <juanpabloramosp@hotmail.com>
2024-05-28 19:06:02 -0600

    Dependency injection

--- Northwind202402/BackEnd/Program.cs
+++ Northwind202402/BackEnd/Program.cs
@@ -1,3 +1,9 @@
+using BackEnd.Services.Implementations;
+using BackEnd.Services.Interfaces;
+using DAL.Implementations;
+using DAL.Interfaces;
+using Entities.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region DI
+
+builder.Services.AddDbContext<NorthWindContext>();
+
+builder.Services.AddScoped<ICategoryDAL, CategoryDALImpl>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+
+
+#endregion
+
+
 
 var app = builder.Build();
 
