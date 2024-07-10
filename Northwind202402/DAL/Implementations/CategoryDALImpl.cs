5363cc01c3fba35be5e9e387a367859f9ccef770
juanpabloramosp <juanpabloramosp@hotmail.com>
2024-07-09 19:09:07 -0600

    Sp de consulta

--- Northwind202402/DAL/Implementations/CategoryDALImpl.cs
+++ Northwind202402/DAL/Implementations/CategoryDALImpl.cs
@@ -20,7 +20,38 @@ namespace DAL.Implementations
             this.context = context;
         }
 
+        public IEnumerable<Category> GetAll()
+        {
+            List<Category> list = new List<Category>();
+            List<sp_GetAllCategories_Result> results;
 
+            string sql = "[dbo].[sp_GetAllCategories]";
+
+            results = context
+                    .Sp_GetAllCategories_Results
+                    .FromSqlRaw(sql)
+                    .ToList();
+
+            foreach (var item in results)
+            {
+                list.Add(
+                    
+                    new Category
+                    {
+                        CategoryId = item.CategoryId,
+                        CategoryName = item.CategoryName,
+                        Description = item.Description,
+                        Picture = item.Picture
+                    }
+                    
+                    
+                    );
+            }
+
+            return list;
+
+
+        }
       
        
     }
--- Northwind202402/Entities/Entities/NorthWindContext.cs
+++ Northwind202402/Entities/Entities/NorthWindContext.cs
@@ -69,6 +69,9 @@ public partial class NorthWindContext : DbContext
 
     public virtual DbSet<Territory> Territories { get; set; }
 
+    public virtual DbSet<sp_GetAllCategories_Result> Sp_GetAllCategories_Results { get; set; }
+
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=.;Database=NorthWind;Integrated Security=True;Trusted_Connection=True; TrustServerCertificate=True;");
